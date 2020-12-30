9bb0efbbf181571689dda1caa6032666bfa20bb1
MForte101 <mark.fortee@gmail.com>
2020-12-30 12:13:28 -0500

    added side drawer toggle white icon. added two life cycle component methods to prevent order summary re-rendering every click by coupling it with the modal pop-up

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Auxillary from '../../../hoc/Auxillary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    return (
-        <Auxillary>
-        <Backdrop show={props.show} click={props.modalClosed} />
-        <div style={{
-            display: props.show ? 'block': 'none',
-        }} className={classes.Modal}>
-        {props.children}
-        </div>
-        </Auxillary>
-    );
-};
+class Modal extends Component {
+
+    componentDidUpdate() {
+        console.log('Modal updated');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <Auxillary>
+            <Backdrop show={this.props.show} click={this.props.modalClosed} />
+            <div style={{
+                display: this.props.show ? 'block': 'none',
+            }} className={classes.Modal}>
+            {this.props.children}
+            </div>
+            </Auxillary>
+        );
+    }
+}
 
 
-export default modal;
+export default Modal;
